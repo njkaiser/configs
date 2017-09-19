5c610049971d04dd2e25b6231710193e4cf82c5e
Nate Kaiser <natejkaiser@gmail.com>
2017-09-19 10:44:55 -0500

    finally got annoyed enough to create install script - will add more as I remember them

---
+++ install.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# install byobu
+sudo apt-get install -y byobu
+
+# install tree
+sudo apt-get install -y tree
+
+# install vim
+sudo apt-get install -y vim
+
+# install atom
+sudo add-apt-repository -y ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install -y atom
+
+# install ag
+sudo apt-get install -y silversearcher-ag
+
+# install ack-grep
+sudo apt-get install -y ack-grep
