7897af7d439f004ec3a30d9bbeddead861b45437
Nate Kaiser <natejkaiser@gmail.com>
2017-09-22 10:07:19 -0500

    added spotify to install script

--- install.bash
+++ install.bash
@@ -27,8 +27,13 @@ sudo apt-get install -y silversearcher-ag
 sudo apt-get install -y ack-grep
 
 # install git:
-sudo apt-get install git
+sudo apt-get install -y git
 
 # install gitk:
-sudo apt-get install gitk
+sudo apt-get install -y gitk
 
+# install spotify:
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install -y spotify-client
