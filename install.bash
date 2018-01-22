0c4516e15e3599981dd9e66e09658a8dc4ced576
Nate Kaiser <nate@zoox.com>
2018-01-22 14:33:31 -0800

    added simplescreenrecorder

--- install.bash
+++ install.bash
@@ -95,3 +95,7 @@ sudo apt-get install -y vlc
 
 # useful graphical tool for modifying PDFs:
 # sudo apt-get install -y pdfshuffler 
+
+sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder
+sudo apt-get update
+sudo apt-get install -y simplescreenrecorder
