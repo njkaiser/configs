9080e9d60d42ac514532523953cb031e10e68bf6
Nate Kaiser <nate@zoox.com>
2018-09-19 22:30:43 -0700

    back at it

--- setup.bash
+++ setup.bash
@@ -1,22 +1,20 @@
 #!/bin/bash
 set -x
 
-# SCRATCH THIS, BETTER TO SYMLINK
-# move all files from ~/config/* to ~/*
-# for file in $HOME/config/{..?,.[!.],}*; do
-#     mv $file $HOME
-# done
-
 # add symlinks for necessary config files
 cd ~/
-ln -s ~/configs/.vimrc .
-ln -s ~/configs/.bash_aliases .
+ln -s ~/configs/.bash_aliases
+ln -s ~/configs/.gitignore_global
+ln -s ~/configs/.nanorc
+ln -s ~/configs/.tmux.conf
+ln -s ~/configs/.vimrc
 
 # set git to use global gitignore file:
 git config --global core.excludesfile ~/.gitignore_global
 git config --global user.name "Nate Kaiser"
 git config --global user.email "natejkaiser@gmail.com"
 git config --global push.default simple
+git config --global remote.origin.prune true
 
 # set desktop background to most awesome picture in the world:
 DIR="$HOME/Pictures"
