e586bc11547aafa20c96811c0e0ebdf7d1ccacb9
Nate Kaiser <natejkaiser@gmail.com>
2018-09-23 14:49:14 -0700

    reorging things

--- setup.bash
+++ setup.bash
@@ -3,13 +3,13 @@ set -x
 
 # add symlinks for necessary config files
 cd ~/
-ln -s ~/configs/.bash_aliases
-ln -s ~/configs/.gitignore_global
-ln -s ~/configs/.nanorc
+rm ~/.bash_aliases; ln -s ~/configs/.bash_aliases ~/.bash_aliases
+rm ~/.gitignore_global; ln -s ~/configs/.gitignore_global ~/.gitignore_global
+ln -s ~/configs/.nanorc ~/.nanorc
 ln -s ~/configs/.tmux.conf
 ln -s ~/configs/.vimrc
 
-# set git to use global gitignore file:
+# set git to use some handy global config stuff:
 git config --global core.excludesfile ~/.gitignore_global
 git config --global user.name "Nate Kaiser"
 git config --global user.email "natejkaiser@gmail.com"
@@ -17,6 +17,6 @@ git config --global push.default simple
 git config --global remote.origin.prune true
 
 # set desktop background to most awesome picture in the world:
-DIR="$HOME/Pictures"
-PIC="$DIR/sdoEquinox_0171_fixed.png"
-gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
+# DIR="$HOME/Pictures"
+# PIC="$DIR/sdoEquinox_0171_fixed.png"
+# gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
